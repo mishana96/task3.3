c8be04b4dddc7b65c358a0bcaa51c241f02f6d57
Mikheev Nikita <mishana32@gmail.com>
2021-09-12 16:42:29 +0500

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,11 @@
+import java.util.Collection;
+import java.util.HashSet;
+
 public class Main {
+//для удаления дубликатов из коллекции достаточно возпользоваться HashSet
+    public static <T>Collection<T> removeDuplicates(Collection<T> collection){
+        return new HashSet<>(collection);
+    }
+
 }
+
